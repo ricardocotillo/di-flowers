9eb156b19ac34f8b23ab8d9f28596257251e66bd
ricardocotillo <ricardo.cotillo@gmail.com>
2022-12-01 22:27:20 -0500

    fixed styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -12,6 +13,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'primary': colors.gray[500],
+        'secondary': colors.gray[600],
+        'dark': colors.gray[900],
+        'light': colors.gray[100],
+        'info': colors.blue[500],
+        'warning': colors.amber[500],
+        'success': colors.green[500],
+        'error': colors.red[500],
         'rose-1': '#e0cdcb',
         'rose-2': '#bb9ba9',
         'rose-3': '#f8f4f2',
