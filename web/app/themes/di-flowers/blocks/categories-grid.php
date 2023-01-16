dcfda57f316cb5166d4a35b4c9a1f041367eb2af
ricardocotillo <ricardo.cotillo@gmail.com>
2023-01-16 14:51:12 -0500

    modify grid

--- web/app/themes/di-flowers/blocks/categories-grid.php
+++ web/app/themes/di-flowers/blocks/categories-grid.php
@@ -2,17 +2,20 @@
 
 use Timber\Timber;
 use Timber\Term;
+use Timber\Post;
 use Timber\Image;
 use Carbon_Fields\Block;
 use Carbon_Fields\Field;
 
 Block::make('Categories Grid')
   ->add_fields( array(
-    Field::make( 'association', 'df_categories_grid', 'Categories Grid' )
+    Field::make( 'association', 'df_categories_grid', 'Items Grid' )
       ->set_types( array(
           array(
-              'type'		  => 'term',
-              'taxonomy' => 'product_cat',
+              'type'  => 'term',
+          ),
+          array(
+              'type'  => 'post',
           ),
       ) ),
     Field::make( 'text', 'df_grid_cols', 'Columns Number' )
@@ -25,16 +28,24 @@ Block::make('Categories Grid')
   ->set_render_callback( function( $fields, $attributes, $inner_blocks ) {
     $context = Timber::context();
     $context['attrs'] = join( ' ', $attributes);
-    $ids = array_map( function($c) {
-        return $c['id'];
+    $context['categories'] = array_map( function($c) {
+      switch ($c['type']) {
+        case 'term':
+          $obj = new Term( $c['id'] );
+          return [
+            'obj'   => $obj,
+            'image' => new Image( $obj->meta( 'thumbnail_id' ) ),
+          ];
+          break;
+        default:
+          $obj = new Post( $c['id'] );
+          return [
+            'obj'   => $obj,
+            'image' => $obj->thumbnail(),
+          ];
+          break;
+      }
     }, $fields[ 'df_categories_grid' ] );
-    $context['categories'] = array_map( function($id) {
-      $thumb_id = get_woocommerce_term_meta( $id, 'thumbnail_id', true );
-      return [
-        'category'  => new Term($id),
-        'image'     => new Image($thumb_id),
-      ];
-    }, $ids );
     $context['template_cols'] = join( ' ' , array_map( function() {
       return '1fr';
     }, range( 0, (int)$fields[ 'df_grid_cols' ] - 1 ) ) );
