ff438e59e4726a8cf6123abfe216b988a954cfd3
ricardocotillo <ricardo.cotillo@gmail.com>
2023-05-27 10:50:56 -0500

    fixes

--- web/app/themes/di-flowers/src/app.js
+++ web/app/themes/di-flowers/src/app.js
@@ -1,5 +1,24 @@
+import AirDatepicker from 'air-datepicker'
+import localeEs from 'air-datepicker/locale/es';
+import { addDays } from 'date-fns'
 import Alpine from 'alpinejs'
+
+import 'air-datepicker/air-datepicker.css';
 
 window.Alpine = Alpine
 
 Alpine.start()
+
+window.onload = () => {
+  const dfInput = document.querySelector('.df-datepicker input')
+  if (dfInput) {
+    const d = new Date()
+    const startDate = addDays(d, 2)
+    const airdatepicker = new AirDatepicker(`#${dfInput.id}`, {
+      locale: localeEs,
+      startDate,
+      minDate: startDate,
+      selectedDates: [startDate]
+    })
+  }
+}
