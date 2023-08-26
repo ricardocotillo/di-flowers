c9dea6978012b2572e3d46b487d5f4ea515a1ac5
ricardocotillo <ricardo.cotillo@gmail.com>
2023-08-26 16:39:37 -0500

    changes

--- web/app/themes/di-flowers/woocommerce.php
+++ web/app/themes/di-flowers/woocommerce.php
@@ -2,11 +2,6 @@
 
 use Timber\Integrations\WooCommerce\WooCommerce;
 
-// $data = carbon_get_theme_option('df_categories');
-// $ids = array_map(fn($d) => (int)$d['id'], $data);
-// $categories = get_terms(['include' => $ids]);
-// wc_add_notice( __("Your payment has been successful", "woocommerce"), "success" );
-
 $facet_cats = facetwp_display('facet','categories');
 $reset = facetwp_display('facet', 'reset');
 $context = [
